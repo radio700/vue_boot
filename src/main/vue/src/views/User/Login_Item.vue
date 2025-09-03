4068b69c815658b04c6c4a4804f2b14670fb00b4
SejungKim <radio700@hanmail.net>
2025-09-03 17:53:49 +0900

    teset

---
+++ src/main/vue/src/views/User/Login_Item.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from "vue";
+
+import axios from "axios";
+
+const id = ref("");
+const password = ref("");
+
+const login = function () {
+  console.log("🚀 ~ password:", password)
+
+
+  axios.post("/api/login", {
+    id : "a",
+    password: "B",
+  });
+
+};
+</script>
+
+<template>
+  <div>
+    <div>
+      <input type="text" name="id" v-model="id" />
+    </div>
+    <div>
+      <input type="text" name="password" v-model="password" />
+    </div>
+    <div>
+      <button @click="login">로그인</button>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
